e63f2bd335a9c9b5ba830a4608d0d3ca9fecb132
shh <dancealoha07@gmail.com>
2025-11-26 20:43:24 +0300

    Create ProgramConfig.cs

---
+++ ProgramConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ProgramConfig
+    {
+        public const string version = "1.0.0";
+        public const string DeveloperName = "Linux";
+
+        public static void ProgramInfo()
+        {
+            Console.WriteLine($"Весрия: {version} , разработчик: {DeveloperName}");
+        }
+    }
+}
