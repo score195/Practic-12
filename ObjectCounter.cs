07355bd35d5da00282996e2b2cd8b9040059cdd0
shh <dancealoha07@gmail.com>
2025-11-26 20:40:19 +0300

    Create ObjectCounter.cs

---
+++ ObjectCounter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class ObjectCounter
+    {
+        private static int counter = 0;
+
+        public ObjectCounter()
+        {
+            counter++;
+        }
+        public static int GetCounter()
+        {
+            return counter;
+        }
+    }
+}
