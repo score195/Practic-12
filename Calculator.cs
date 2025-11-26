72e02edb80159a22410f607ac1f724b490701b2e
shh <dancealoha07@gmail.com>
2025-11-26 20:35:54 +0300

    Create Calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practic12
+{
+    internal class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Ошибка! делить на ноль нельзя");
+            }
+            return a / b;
+        }
+    }
+}
