6395b1d31c50030ba664f376c29d7202ea2218c8
shh <dancealoha07@gmail.com>
2025-11-26 20:44:17 +0300

    Create Settings.cs

---
+++ Settings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Settings
+    {
+        private static readonly string configPath;
+
+        static Settings()
+        {
+            configPath = "C:\\Program Files\\settings\\config.txt";
+        }
+        public static void PrintConfigPath()
+        {
+            Console.WriteLine($"Путь к файлу конфигурации: {configPath}");
+        }
+    }
+}
